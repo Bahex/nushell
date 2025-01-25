bd202396d2444795d47757736127f1544914e319
Bahex <bahey1999@gmail.com>
2025-01-25 05:45:11 +0300

    feat(attr example): allow blocks as example code

--- crates/nu-cmd-lang/src/core_commands/attr/example.rs
+++ crates/nu-cmd-lang/src/core_commands/attr/example.rs
@@ -26,7 +26,11 @@ impl Command for AttrExample {
                 SyntaxShape::String,
                 "Description of the example.",
             )
-            .required("example", SyntaxShape::String, "Example code snippet.")
+            .required(
+                "example",
+                SyntaxShape::OneOf(vec![SyntaxShape::Block, SyntaxShape::String]),
+                "Example code snippet.",
+            )
             .named(
                 "result",
                 SyntaxShape::Any,
@@ -48,12 +52,32 @@ impl Command for AttrExample {
         _input: PipelineData,
     ) -> Result<PipelineData, ShellError> {
         let description: Spanned<String> = call.req(engine_state, stack, 0)?;
-        let code: Spanned<String> = call.req(engine_state, stack, 1)?;
         let result: Option<Value> = call.get_flag(engine_state, stack, "result")?;
+        let example = call
+            .positional_nth(stack, 1)
+            .expect("checked through parser");
+
+        let example_content = match example.as_block() {
+            Some(block_id) => {
+                let block = engine_state.get_block(block_id);
+                let contents =
+                    engine_state.get_span_contents(block.span.expect("a block must have a span"));
+                let contents = contents
+                    .strip_prefix(b"{")
+                    .and_then(|x| x.strip_suffix(b"}"))
+                    .unwrap_or(contents)
+                    .trim_ascii();
+                String::from_utf8_lossy(contents).into_owned()
+            }
+            None => match example.as_string() {
+                Some(v) => v,
+                None => panic!("internal error: missing block"),
+            },
+        };
 
         let mut rec = record! {
             "description" => Value::string(description.item, description.span),
-            "example" => Value::string(code.item, code.span),
+            "example" => Value::string(example_content, example.span),
         };
         if let Some(result) = result {
             rec.push("result", result);
@@ -69,12 +93,33 @@ impl Command for AttrExample {
         _input: PipelineData,
     ) -> Result<PipelineData, ShellError> {
         let description: Spanned<String> = call.req_const(working_set, 0)?;
-        let code: Spanned<String> = call.req_const(working_set, 1)?;
         let result: Option<Value> = call.get_flag_const(working_set, "result")?;
+        let example_expr = call
+            .assert_ast_call()?
+            .positional_nth(1)
+            .expect("checked through parser");
+
+        let example_content = match example_expr.as_block() {
+            Some(block_id) => {
+                let block = working_set.get_block(block_id);
+                let contents =
+                    working_set.get_span_contents(block.span.expect("a block must have a span"));
+                let contents = contents
+                    .strip_prefix(b"{")
+                    .and_then(|x| x.strip_suffix(b"}"))
+                    .unwrap_or(contents)
+                    .trim_ascii();
+                String::from_utf8_lossy(contents).into_owned()
+            }
+            None => {
+                let example: String = call.req_const(working_set, 1)?;
+                example
+            }
+        };
 
         let mut rec = record! {
             "description" => Value::string(description.item, description.span),
-            "example" => Value::string(code.item, code.span),
+            "example" => Value::string(example_content, example_expr.span),
         };
         if let Some(result) = result {
             rec.push("result", result);
@@ -84,6 +129,10 @@ impl Command for AttrExample {
     }
 
     fn is_const(&self) -> bool {
+        true
+    }
+
+    fn requires_ast_for_arguments(&self) -> bool {
         true
     }
 }
