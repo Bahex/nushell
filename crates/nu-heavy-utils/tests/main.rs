d835f37a9f4eae15eca85783d1ba7eb29a387639
Bahex <17417311+Bahex@users.noreply.github.com>
2026-05-12 10:52:39 +0300

    test: use custom test harness in nu-heavy-utils

--- crates/nu-heavy-utils/tests/main.rs
+++ crates/nu-heavy-utils/tests/main.rs
@@ -1,11 +1,6 @@
-pub mod endian;
+#![allow(clippy::unwrap_used)]
 
-pub use endian::Endian;
-
-#[cfg(test)]
 #[allow(unused_imports)]
 #[macro_use]
 extern crate nu_test_support;
-
-#[cfg(test)]
 use nu_test_support::harness::main;
